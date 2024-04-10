a3840459d93afff3cdc62de62695fdcb1d95b739
Junting <chen112p@gmail.com>
2024-04-10 06:35:47 -0400

    can run distilroberta local, need to test cuda

---
+++ metrics/eval.py
@@ -0,0 +1,30 @@
+import torch
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+def accuracy(output, target):
+    """Computes the precision@k for the specified values of k"""
+    batch_size = target.shape[0]
+
+    _, pred = torch.max(output, dim=-1)
+
+    correct = pred.eq(target).sum() * 1.0
+
+    acc = correct / batch_size
+
+    return acc
---
+++ models/distilroberta_classifier.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import AutoModel
+import torch.nn.functional as F
+
+class RobertaClassifier(torch.nn.Module):
+    def __init__(self,
+                dropout_rate):
+        super().__init__()
+        self.num_class = 2
+        self.pretrain_model = AutoModel.from_pretrained('distilroberta-base', return_dict = True)
+        self.hidden = torch.nn.Linear(self.pretrain_model.config.hidden_size, 
+                                self.pretrain_model.config.hidden_size)
+        self.classifier = torch.nn.Linear(self.pretrain_model.config.hidden_size,
+                                        self.num_class)
+        torch.nn.init.xavier_uniform_(self.hidden.weight)
+        torch.nn.init.xavier_uniform_(self.classifier.weight)
+        self.dropout = torch.nn.Dropout(dropout_rate)
+
+    def forward(self, 
+                input_ids,
+                attention_mask):
+        # roberta model
+        output = self.pretrain_model(input_ids = input_ids, attention_mask = attention_mask)
+        pooled_output = torch.mean(output.last_hidden_state, 1)
+        # neural network classification layer
+        pooled_output = self.hidden(pooled_output)
+        pooled_output = self.dropout(pooled_output)
+        pooled_output = F.relu(pooled_output)
+        logits = self.classifier(pooled_output)        
+        return(logits)
